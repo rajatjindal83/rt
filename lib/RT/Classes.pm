bcf0ba43bae17d62340a904084e8a36da14f558c
Alex Vandiver <alexmv@bestpractical.com>
2014-10-06 16:46:54 -0400

    Simplify ->Next logic

--- lib/RT/Classes.pm
+++ lib/RT/Classes.pm
@@ -73,23 +73,10 @@ Returns the next Object that this user can see.
 sub Next {
     my $self = shift;
 
-
     my $Object = $self->SUPER::Next();
-    if ((defined($Object)) and (ref($Object))) {
-   if ( $Object->CurrentUserHasRight('SeeClass') ) {
-        return($Object);
-    }
-
-    #If the user doesn't have the right to show this Object
-    else {
-        return($self->Next());
-    }
-    }
-    #if there never was any Object
-    else {
-    return(undef);
-    }
-
+    return undef unless defined $Object;
+    return $self->Next unless $Object->CurrentUserHasRight('SeeClass');
+    return $Object;
 }
 
 sub _SingularClass { "RT::Class" }
